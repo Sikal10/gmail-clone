68dcc55bab01f25c25b60b3ea3fd6e9547f23aa4
Sikal Sikal <sikal.sikal.ss@gmail.com>
2021-01-11 21:58:28 +0100

    created project with create react app and added header files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="app">
+        <Header />
       <h1>Let's build gmail</h1>
     </div>
   );
