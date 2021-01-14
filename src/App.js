b52cb4018a092ce61f326e94e9d82cdc38fc841f
Sikal Sikal <sikal.sikal.ss@gmail.com>
2021-01-14 16:07:06 +0100

    Added the add and remove message from firebase functionality. Also, authentication with google was added.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import EmailList from "./components/EmailList/EmailList";
+import Mail from "./components/Mail/Mail";
+import SendMail from "./components/SendMail/SendMail";
+import {useDispatch, useSelector} from "react-redux";
+import {selectSendMessageIsOpen} from "./features/mailSlice";
+import {selectUser} from "./features/userSlice";
+import Login from "./components/Login/Login";
+import {auth} from "./firebase";
+import {login} from "./features/userSlice";
 
 function App() {
-  return (
-    <div className="app">
-        <Header />
-      <h1>Let's build gmail</h1>
-    </div>
-  );
+    const sendMessageIsOpen = useSelector(selectSendMessageIsOpen)
+    const user = useSelector(selectUser)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            if (user) {
+                dispatch(login({
+                    displayName: user.displayName,
+                    email: user.email,
+                    photoUrl: user.photoURL
+                }))
+            } else {
+                throw new Error("There is no user")
+            }
+        })
+    }, [])
+
+    return (
+        <Router>
+            {!user ? <Login />:   <div className="app">
+                <Header/>
+
+                <div className="app__body">
+                    <Sidebar/>
+
+                    <Switch>
+                        <Route component={Mail} path={"/mail"}/>
+                        <Route component={EmailList} path={"/"} />
+                    </Switch>
+                </div>
+                {sendMessageIsOpen && <SendMail />}
+            </div>}
+
+        </Router>
+    );
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,48 @@
 import React from 'react';
 import "./Header.css"
 import MenuIcon from '@material-ui/icons/Menu';
-import {IconButton} from "@material-ui/core";
+import {IconButton, Avatar} from "@material-ui/core";
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import {useDispatch, useSelector} from "react-redux";
+import {selectUser, logout} from "../../features/userSlice";
+import {auth} from "../../firebase";
 
 const Header = () => {
+    const user = useSelector(selectUser);
+    const dispatch = useDispatch();
+
+    const signOut = async () => {
+        const loggedInUser = await auth.signOut();
+        dispatch(logout(loggedInUser))
+    }
+
     return (
         <div className={"header"}>
             <div className="header__left">
                 <IconButton>
                     <MenuIcon />
                 </IconButton>
+                <img src="https://www.amocrm.com/static/images/pages/integrations/logo/gmail.png" alt=""/>
             </div>
-            <div className="header__middle"></div>
-            <div className="header__right"></div>
+
+            <div className="header__middle">
+                <SearchIcon />
+                <input placeholder={"Search mail"}/>
+                <ArrowDropDownIcon className={"header__inputCaret"} />
+            </div>
+
+            <div className="header__right">
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsIcon />
+                </IconButton>
+                <Avatar onClick={signOut} src={user?.photoUrl} />
+            </div>
         </div>
     );
 };
